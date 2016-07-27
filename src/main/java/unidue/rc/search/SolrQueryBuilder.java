7f7bc671f6b4d714ce9aec75c9fbca262ee90c65
nils <nils.verheyen@gmail.com>
2016-07-27 11:34:31 +0200

    remove special chars when trimming search values

--- src/main/java/unidue/rc/search/SolrQueryBuilder.java
+++ src/main/java/unidue/rc/search/SolrQueryBuilder.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2014 - 2016 Universitaet Duisburg-Essen (semapp|uni-due.de)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,16 +16,14 @@
 package unidue.rc.search;
 
 
-import org.apache.commons.lang3.CharEncoding;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 /**
  * A <code>SolrQueryBuilder</code> is a convenience class to build {@link org.apache.solr.client.solrj.SolrQuery}.
@@ -38,8 +36,8 @@ public class SolrQueryBuilder {
 
     private static final Logger LOG = LoggerFactory.getLogger(SolrQueryBuilder.class);
 
-    private static final String DIVIDER = ":";
-    private static final String WILDCARD_SIGN = "*";
+    private static final char DIVIDER = ':';
+    private static final char WILDCARD_SIGN = '*';
 
     private Map<String, SolrQuery.ORDER> sortFields = new LinkedHashMap<>();
 
@@ -219,7 +217,7 @@ public class SolrQueryBuilder {
         }
         query.setQuery(queryStringBuilder.toString());
         if (queryStringBuilder.length() == 0)
-            query.setQuery(WILDCARD_SIGN);
+            query.setQuery(Character.toString(WILDCARD_SIGN));
 
         return query;
     }
@@ -242,8 +240,15 @@ public class SolrQueryBuilder {
         if (value == null)
             return null;
 
-        value = StringUtils.removePattern(value, "^([\\s\\" + WILDCARD_SIGN + "])*");
-        value = StringUtils.removePattern(value, "([\\s\\" + WILDCARD_SIGN + "])*$");
-        return value;
+        StringBuilder b = new StringBuilder();
+        IntStream.range(0, value.length())
+                .forEach(i -> b.append(getEscapedString(value.charAt(i))));
+        return b.toString();
+    }
+
+    private String getEscapedString(char c) {
+        return c == WILDCARD_SIGN || c == DIVIDER
+               ? StringUtils.EMPTY
+               : Character.toString(c);
     }
 }
